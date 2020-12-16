6baae51646fd3446a748d8f5f73d55adfd6495c4
Evgenii Shumilov <mechnotech@yandex.ru>
2020-12-16 19:38:13 +0300

    first ticker

---
+++ tickers.py
@@ -0,0 +1,25 @@
+import os
+
+import PublicApiClient as NtApi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pub_ = os.getenv('TN_PUB_KEY')
+sec_ = os.getenv('TN_PRIVATE_KEY')
+
+res = NtApi.PublicApiClient(pub_, sec_, NtApi.PublicApiClient().V2)
+
+
+def get_ticker(ticker, sup=True):
+    cmd_ = 'getSecurityInfo'
+    param_ = {
+        'ticker': ticker,
+        'sup': sup
+    }
+    res = NtApi.PublicApiClient(pub_, sec_, NtApi.PublicApiClient().V1)
+    return res.sendRequest(cmd_, param_).content.decode("utf-8")
+
+
+if __name__ == '__main__':
+    print(get_ticker('SBER'))
